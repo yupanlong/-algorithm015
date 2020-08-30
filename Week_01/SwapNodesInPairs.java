6a96260e06dbc480f88e65c547c6f97219f7e936
yupanlong <152225495339@163.com>
2020-08-30 18:07:36 +0800

    完成算法 -p189 两两交换链表中的节点

---
+++ Week_01/SwapNodesInPairs.java
@@ -0,0 +1,19 @@
+public class SwapNodesInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next ==null) {
+            return  head;
+        }
+        ListNode firstNode =  head;
+        ListNode secondNode = head.next;
+
+        firstNode.next = swapPairs(secondNode.next);
+
+        secondNode.next = firstNode ;
+        return secondNode;
+
+    }
+
+
+
+}
